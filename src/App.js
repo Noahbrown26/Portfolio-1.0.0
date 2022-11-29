a5d745cd4822ca2d8336a7f748aac5d17f4e82ed
Noahb26 <noahbrown2663@gmail.com>
2022-11-28 19:41:42 -0800

    add component for sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Navbar from "./components/Navbar";
-/*import useMediaQuery from "./hooks/useMediaQuery";*/
-import { useState } from "react";
+import DotGroup from "./components/DotGroup";
+import useMediaQuery from "./hooks/useMediaQuery";
+import { useState, useEffect } from "react";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
-  const [isTopOfPage] = useState(true);
-  /*const isDesktop = useMediaQuery("(min-width: 1060px)");*/
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
 
   return (
     <div className="app bg-deep-blue">
@@ -14,6 +28,14 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+       <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup 
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+          />
+        )}
+       </div>
     </div>
   );
 }
